37b8b021e95d844c6478145fd2bf30b11eb4de15
Giovanni <giovidecaro@live.it>
2022-10-26 11:58:43 +0200

    hero section to complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,7 +54,51 @@ function onClick(e){
         document.body.style.backgroundColor = "#111110";
         shadowOn = true;
    }
-
-
 }
 
+// hidden value 
+let hidden = true;
+
+const hiddenObj = document.getElementsByClassName('hidden');
+
+const hiddenObjArray = Array.from(hiddenObj);
+
+hiddenObjArray.forEach(element => {
+   element.style.opacity = "0";
+})
+
+// see the hidden object 
+const downArrowIcon = document.querySelectorAll('.down-arrow-icon');
+// change 1st choice border radius
+
+
+downArrowIcon.forEach(arrow => {
+   arrow.addEventListener('click', showHidden)
+})
+
+function showHidden(){
+   console.log('ciao');
+   if(hidden){
+      hiddenObjArray.forEach(element => {
+         element.style.opacity = "100";
+
+         document.getElementById('first-left-value').style.borderRadius = "10px 10px 0 0";
+         document.getElementById('first-right-value').style.borderRadius = "10px 10px 0 0";
+         document.getElementById('left-mooney-input').style.borderRadius = "0 0 10px 10px";
+         document.getElementById('right-mooney-input').style.borderRadius = "0 0 10px 10px";
+      })
+      hidden = false;
+   } else {
+      hiddenObjArray.forEach(element => {
+         element.style.opacity = "0";
+
+         document.getElementById('first-left-value').style.borderRadius = "10px";
+         document.getElementById('first-right-value').style.borderRadius = "10px";
+         document.getElementById('left-mooney-input').style.borderRadius = "10px";
+         document.getElementById('right-mooney-input').style.borderRadius = "10px";
+      })
+      hidden = true;
+   }
+
+   
+}
